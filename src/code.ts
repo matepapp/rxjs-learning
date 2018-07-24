8e6a2708c6dc6057a3b9eb7d40b82b2ec81f411a
Mate Papp <matepapp@icloud.com>
2018-07-24 15:48:58 +0200

    Create Observable stream and show values on list

--- src/code.ts
+++ src/code.ts
@@ -1,3 +1,14 @@
-import { map } from 'rxjs/operators';
+import { Observable } from 'rxjs';
 
-console.log(map);
+let observable = Observable.create((observer: any) => {
+    observer.next('Hey guys');
+});
+
+observable.subscribe((next: any) => addItem(next));
+
+function addItem(value: any) {
+    var node = document.createElement("li");
+    var textNode = document.createTextNode(value);
+    node.appendChild(textNode);
+    document.getElementById("output").appendChild(node);
+}

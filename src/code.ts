d5d5b9ab27019bdba7e53eb2a49e762779bb3aaa
Mate Papp <matepapp@icloud.com>
2018-07-24 15:51:49 +0200

    Extract with completed

--- src/code.ts
+++ src/code.ts
@@ -1,10 +1,17 @@
-import { Observable } from 'rxjs';
+import { Observable, Observer } from 'rxjs';
 
-let observable = Observable.create((observer: any) => {
-    observer.next('Hey guys');
+let observable = Observable.create((observer: Observer<any>) => {
+    observer.next('Hey guys!');
+    observer.next('How are you?');
+    observer.complete()
+    observer.next('This item will not send.');
 });
 
-observable.subscribe((next: any) => addItem(next));
+observable.subscribe(
+    (next: any) => addItem(next),
+    (error: any) => addItem(error),
+    () => addItem('Completed!')
+);
 
 function addItem(value: any) {
     var node = document.createElement("li");
